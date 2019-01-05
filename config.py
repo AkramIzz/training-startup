806f029de6b8ca4e87258caae4e806b529a9fede
Akram Izzeldin <akram.izz.kh@gmail.com>
2019-01-05 15:55:54 +0200

    Changed to a MySQL database inplace of the SQLite database

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "my-secret-key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://alpha:alpha@localhost/app'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ADMIN_SWATCH = 'cosmo'
     POSTS_PER_PAGE = 3
